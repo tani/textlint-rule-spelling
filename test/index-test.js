60caef6dafefeccfbf13a3db2bc51e6bb6d01cfa
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-04-18 19:09:50 +0900

    Add a test for skipPatterns

--- test/index-test.js
+++ test/index-test.js
@@ -3,41 +3,51 @@ const TextLintTester = require("textlint-tester");
 const tester = new TextLintTester();
 const rule = require("../src/index");
 
-tester.run("spelling", rule, {
-    valid: [
-        "text"
-    ],
+tester.run(
+  "spelling",
+  {
+    rules: [
+      {
+        ruleId: "spelling",
+        rule,
+        options: { language: "en-us", skipPatterns: ["npm"] }
+      }
+    ]
+  },
+  {
+    valid: ["text", "npm"],
     invalid: [
-        {
-            text: "It is colour.",
-	    output: "It is color.",
-            errors: [
-                {
-                    message: "colour -> color",
-                    line: 1,
-                    column: 7
-                }
-            ]
-        },
-        {
-            text: `It has many colour.
+      {
+        text: "It is colour.",
+        output: "It is color.",
+        errors: [
+          {
+            message: "colour -> color",
+            line: 1,
+            column: 7
+          }
+        ]
+      },
+      {
+        text: `It has many colour.
 
 One more colour`,
-            output: `It has many color.
+        output: `It has many color.
 
 One more color`,
-            errors: [
-                {
-                    message: "colour -> color",
-                    line: 1,
-                    column: 13
-                },
-                {
-                    message: "colour -> color",
-                    line: 3,
-                    column: 10
-                }
-            ]
-        }
+        errors: [
+          {
+            message: "colour -> color",
+            line: 1,
+            column: 13
+          },
+          {
+            message: "colour -> color",
+            line: 3,
+            column: 10
+          }
+        ]
+      }
     ]
-});
+  }
+);
