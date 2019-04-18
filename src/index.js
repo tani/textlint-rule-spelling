c06ba52b2f942554feaf071e73885b23ff45f6fe
TANIGUCHI Masaya <xtaniguchimasaya+code@gmail.com>
2019-04-18 20:10:48 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /*
- * This file is part of textlint-plugin-latex2e
+ * This file is part of textlint-rule-spelling
  *
  * textlint-rule-spelling is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * textlint-plugin-latex2e is distributed in the hope that it will be useful,
+ * textlint-rule-spelling is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
