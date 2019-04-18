53f3df734c9b00ead098365693d1ac494a3cd9b1
TANIGUCHI Masaya <xtaniguchimasaya+code@gmail.com>
2019-04-18 20:05:55 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+/*
+ * This file is part of textlint-plugin-latex2e
+ *
+ * textlint-rule-spelling is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * textlint-plugin-latex2e is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with textlint-plugin-latex2e.  If not, see <http://www.gnu.org/licenses/>.
+ */
 "use strict";
 // eslint-disable-next-line no-unused-vars
 const regeneratorRuntime = require("regenerator-runtime");
